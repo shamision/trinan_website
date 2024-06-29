7bcc49001b855de0105e8256e30eaf889c6edae3
shamision <shami0sion@gmail.com>
2024-06-29 21:23:23 +0200

    Made some changes on the contact form

--- src/components/Sections/Contact/Form.tsx
+++ src/components/Sections/Contact/Form.tsx
@@ -2,11 +2,34 @@
 
 import Image from "next/image"
 import React from "react"
+import { ContactName, ContactEmail } from "@/components/Svgs"
 
 
 export const Form = () => {
     return (
         <div className="w-[600px] h-[630px] bg-white mt-[50px] rounded-[10px]">
+            <div className="ml-[112px] mt-[74px]">
+                <p className="text-customBlue">Your name</p>
+                <div className="ml-[20px] mt-[30px] flex gap-5">
+                    <ContactName />
+                    <input type="text" placeholder="Full name here" className="focus:outline-none text-[#505F98]" required/>
+                </div>
+            </div>
+            <div className="ml-[112px] mt-[40px]">
+                <p className="text-customBlue">Your mail</p>
+                <div className="ml-[20px] mt-[30px] flex gap-5">
+                    <ContactEmail />
+                    <input type="email" placeholder="Your email address" className="focus:outline-none text-[#505F98]" required/>
+                </div>
+            </div>
+            <div className="ml-[112px] mt-[40px]">
+                <p className="text-customBlue">Message</p>
+                <input type="text" placeholder="Type your message here" className="ml-[20px] mt-[30px] flex gap-5 focus:outline-none text-[#505F98]" required/>
+            </div>
+
+            <button className="bg-customBlue w-[180px] h-[60px] rounded-md ml-[112px] mt-[130px]">Send Message</button>
         </div>
+        
+        
     );
 };
